16025b411235a85b800c4e8f26715ebd430a966e
Edenilson Pineda <calberto@hightech-corp.com>
2022-02-21 13:09:51 -0600

    add inheritance example and reformat README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # acloudguru-python-introduction
+
+> Recommended IDE - [PyCharm Community Edition](https://www.jetbrains.com/es-es/pycharm/download/)
+
+### Development Tools
+| Tool             | Url                                                 |
+|------------------|-----------------------------------------------------|
+| Python v3.7      | https://www.python.org/downloads/release/python-370 |
+| Ubuntu 20.04 LTS | https://ubuntu.com/download/desktop                 |
+
+### Useful resources:
+- [Python Official Documentation](https://docs.python.org/3/)
+- [Python Modules](https://docs.python.org/3/py-modindex.html)
--- tire_composition.py
+++ tire_composition.py
@@ -27,8 +27,8 @@ class Tire:
         >>> tire.circumference()
         80.1
         """
-        side_wall_inches = (self.width * (self.ratio / 100)) / 25.4
-        total_diameter = side_wall_inches * 2 + self.diameter
+        # side_wall_inches = (self.width * (self.ratio / 100)) / 25.4
+        total_diameter = self._side_wall_inches() * 2 + self.diameter
         return round(total_diameter * pi, 1)
 
     def __repr__(self):
@@ -41,3 +41,27 @@ class Tire:
         """
         return (f"{self.tire_type}{self.width}/{self.ratio}"
                 + f"{self.construction}{self.diameter}")
+
+    def _side_wall_inches(self):
+        return (self.width * (self.ratio / 100)) / 25.4
+
+
+class SnowTire(Tire):
+    def __init__(self, tire_type, width, ratio, diameter, chain_thickness, brand='', construction='R'):
+        super(SnowTire, self).__init__(tire_type, width, ratio, diameter, brand, construction)
+        self.chain_thickness = chain_thickness
+
+    def circumference(self):
+        """
+        The circumference of a tire w/ snow chains in inches
+
+        >>> tire = SnowTire('P',205,65,15,2)
+        >>> tire.circumference()
+        92.7
+        """
+        total_diameter = (self._side_wall_inches() + self.chain_thickness) * 2 + self.diameter
+        return round(total_diameter * pi, 1)
+
+    def __repr__(self):
+        return super(SnowTire, self).__repr__() + " (Snow)"
+
