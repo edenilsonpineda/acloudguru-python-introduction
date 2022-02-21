2cea76b82caa600309bac6d6a1bd291e15d77f87
Edenilson Pineda <calberto@hightech-corp.com>
2022-02-21 01:28:36 -0600

    add doctest and modify composition class example

--- car.py
+++ car.py
@@ -3,5 +3,15 @@ class Car:
     Car models a car w/ tires and an engine
     """
 
-    def __init__(self):
-        pass  # Not implementing anything
+    def __init__(self, engine, tires):
+        self.engine = engine
+        self.tires = tires
+
+    def description(self):
+        print(f"A car with an {self.engine} engine, and {self.tires} tires")
+
+    def wheel_circumference(self):
+        if len(self.tires) > 0:
+            return self.tires[0].circumference()
+        else:
+            return 0
--- tire_composition.py
+++ tire_composition.py
@@ -1,3 +1,11 @@
+from math import pi
+
+"""
+@author: edenilsonpineda
+@date: 21-02-2022
+"""
+
+
 class Tire:
 
     def __init__(self, tire_type, width, ratio, diameter, brand='', construction='R'):
@@ -7,6 +15,21 @@ class Tire:
         self.diameter = diameter
         self.brand = brand
         self.construction = construction
+
+    def circumference(self):
+        """
+        The circumference of the tire in inches
+
+        Call this test with:
+        python3.7 -m doctest -v {filename.py}
+
+        >>> tire = Tire('P',205,65,15)
+        >>> tire.circumference()
+        80.1
+        """
+        side_wall_inches = (self.width * (self.ratio / 100)) / 25.4
+        total_diameter = side_wall_inches * 2 + self.diameter
+        return round(total_diameter * pi, 1)
 
     def __repr__(self):
         """
