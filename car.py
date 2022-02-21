227ca19222efea171809f3efcb5e864ef621a5c6
Edenilson Pineda <calberto@hightech-corp.com>
2022-02-21 01:02:56 -0600

    add class example

---
+++ car.py
@@ -0,0 +1,7 @@
+class Car:
+    """
+    Car models a car w/ tires and an engine
+    """
+
+    def __init__(self):
+        pass  # Not implementing anything
--- main.py
+++ main.py
@@ -6,6 +6,10 @@
 
 
 def print_hi(name: object) -> object:
+    """
+
+    :rtype: object
+    """
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
@@ -13,5 +17,3 @@ def print_hi(name: object) -> object:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
