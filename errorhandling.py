089d6448f63c9166718bcdc6c6cabea8bb02202e
Edenilson Pineda <calberto@hightech-corp.com>
2022-02-22 17:59:18 -0600

    add error handling example, raising exceptions

---
+++ errorhandling.py
@@ -0,0 +1,16 @@
+import sys
+
+file_name = 'recipes.txt'
+
+try:
+    my_file = open(file_name, 'x')
+    my_file.write('Spaghetti\n')
+    my_file.close()
+except FileExistsError as err:
+    print(f"The {file_name} file already exists.")
+    sys.exit(1)
+except Exception:
+    print(f"Unable to write to the file")
+    raise
+
+
